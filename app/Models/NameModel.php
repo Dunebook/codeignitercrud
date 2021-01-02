3b39c71f4eb86570946c69b0709efdef09f24d2f
Deven Rathore <hyguyz@outlook.com>
2021-01-02 19:13:22 +0530

    intial commit

---
+++ app/Models/NameModel.php
@@ -0,0 +1,12 @@
+<?php 
+namespace App\Models;
+use CodeIgniter\Model;
+
+class NameModel extends Model
+{
+    protected $table = 'names';
+
+    protected $primaryKey = 'id';
+    
+    protected $allowedFields = ['name', 'email'];
+}
